8a1c6cf71758de45ab14d7f55a6958161cad14bb
Delphine Abouab <dabouab@paul-f4Ar2s1.clusters.42paris.fr>
2024-12-20 16:01:02 +0100

    server on, client connected, msg received

--- Server.cpp
+++ Server.cpp
@@ -6,7 +6,7 @@
 /*   By: dabouab <dabouab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 16:34:28 by dabouab           #+#    #+#             */
-/*   Updated: 2024/12/20 15:26:56 by dabouab          ###   ########.fr       */
+/*   Updated: 2024/12/20 15:59:09 by dabouab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,15 @@ bool	Server::start() {
     proto =  getprotobyname("tcp");
     if (!proto)
         return (notif("Proto failed", false));
+
     // On ouvre la socket de communication
     this->_sfd = socket(PF_INET, SOCK_STREAM, proto->p_proto); 
     sin.sin_family = AF_INET; // precise IPv4 Internet protocols
     sin.sin_port = htons(this->_port); // traduit le port en host to network endian
     // precise que la socket accept les connexions de toutes les IP de la machine (traduit en host to network endian)
     sin.sin_addr.s_addr = htonl(INADDR_ANY); 
-    // On lie la socket a l'adresse
+    
+	// On lie la socket a l'adresse
     if (bind(this->_sfd, (const struct sockaddr *)&sin, sizeof(sin)) < 0)
         notif("Bind failed", false);
     // Precise le nombre de connexion simultanees acceptess par la socket
--- Server.hpp
+++ Server.hpp
@@ -6,7 +6,7 @@
 /*   By: dabouab <dabouab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 11:55:21 by dabouab           #+#    #+#             */
-/*   Updated: 2024/12/20 14:59:48 by dabouab          ###   ########.fr       */
+/*   Updated: 2024/12/20 15:56:59 by dabouab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ public:
 
 	bool	start();
 	void	stop();
+	int		Send();
 	
 	int		getSock_fd();
 };
